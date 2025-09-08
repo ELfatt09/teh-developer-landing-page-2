ddb1b21bc207a7a681a486e8ae6f01c73c3aedde
Consomeatsky <ibnulaginugas@gmail.com>
2025-09-08 14:36:17 +0700

    Commit

---
+++ .history/src/App_20250908143300.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Navbar from './partials/navbar'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+import Portofolio from './page/Portofolio'
+
+function App() {
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+<<<<<<< HEAD
+        <Route path='/portofolio' element={<Portofolio />} />
+      </Routes>
+    </>
+=======
+
+      </Routes>
+
+    </Router>
+>>>>>>> 1868cc9848c2a43694253855b51884a3ec40be9b
+  )
+}
+
+export default App
