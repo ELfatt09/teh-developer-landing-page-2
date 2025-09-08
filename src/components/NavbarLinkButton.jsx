27c62e99c3bf593dfa172cc658df46f68c192af8
Sevalino Elfata <sevalinoelfata8@gmail.com>
2025-09-08 14:34:25 +0700

    fix: Replace anchor tag with Link component for navigation

--- src/components/NavbarLinkButton.jsx
+++ src/components/NavbarLinkButton.jsx
@@ -1,13 +1,14 @@
 import { motion } from "motion/react"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 function NavbarLinkButton({ href, children }) {
   const [isHover, setIsHover] = useState(false)
   return (
     <motion.div onHoverStart={() => setIsHover(true)} onHoverEnd={() => setIsHover(false)} className="flex flex-col items-center space-y-1 cursor-pointer">
-      <a href={href} className={` px-1 rounded-md  font-medium text-lg font-jakarta `}>
+      <Link to={href} className={` px-1 rounded-md  font-medium text-lg font-jakarta `}>
         {children}
-      </a>
+      </Link>
       <motion.div
         animate={{ width: isHover ? "100%" : "0%" }} transition={{ duration: 0.3, ease: "easeInOut" }} layoutId="underline" className={`bg-primary h-1 rounded-full`} />
     </motion.div>
