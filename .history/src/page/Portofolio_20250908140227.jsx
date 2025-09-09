f356bfb76a40b95174250db0dbfa0d864507a467
Sevalino Elfata <sevalinoelfata8@gmail.com>
2025-09-09 14:05:16 +0700

    Refactor Portofolio component: remove unused imports and clean up code structure. Add initial section layout for future development.

--- .history/src/page/Portofolio_20250908140227.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-import Farzan from "../assets/foto-jas/farzan.svg";
-import Farzan from "../assets/foto-jas/farzan.svg";
-import Farzan from "../assets/foto-jas/farzan.svg";
-import Farzan from "../assets/foto-jas/farzan.svg";
--- .history/src/page/Portofolio_20250908140238.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-import Farzan from "../assets/foto-jas/farzan.svg";
-import Zahir from "../assets/foto-jas/farzan.svg";
-import Seavlino from "../assets/foto-jas/farzan.svg";
-import Farzan from "../assets/foto-jas/farzan.svg";
