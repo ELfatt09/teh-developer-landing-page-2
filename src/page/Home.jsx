c4832272eddd3a0570f12b402b7252374cb036dd
Sevalino Elfata <sevalinoelfata8@gmail.com>
2025-09-09 13:58:48 +0700

    `Added Team section to Home page and implemented Team component with TeamCard component`

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Hero from '../sections/Hero'
 import Navbar from '../partials/navbar'
 import AboutUs from '../sections/AboutUs'
+import Team from '../sections/Team'
 
 function Home() {
   return (
       <>
         <Navbar />
         <Hero />
-        <AboutUs />
+      <AboutUs />
+      <Team />
       </>
   )
 }
