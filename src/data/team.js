3f576b074c515e5b6c8a1b9fa3eab43414adab5f
Consomeatsky <ibnulaginugas@gmail.com>
2025-09-10 08:18:04 +0700

    Push

--- src/data/team.js
+++ src/data/team.js
@@ -9,9 +9,14 @@ import SevalinoMenu from "../assets/foto-jas/menu/sevalino-menu.svg";
 import IbnuMenu from "../assets/foto-jas/menu/ibnu-menu.svg";
 
 import IbnuCV from "../assets/cv/cv-ibnu.pdf"
+import FarzanCV from "../assets/cv/cv-farzan.pdf"
 
 import IbnuProject1 from "../assets/projects/ibnu-project-1.png"
 import IbnuProject2 from "../assets/projects/ibnu-project-2.png"
+import ZahirProject1 from "../assets/projects/zahir-project-1.jpg"
+import ZahirProject2 from "../assets/projects/zahir-project-2.jpg"
+import FarzanProject1 from "../assets/projects/farzan-project-1.jpg"
+import FarzanProject2 from "../assets/projects/farzan-project-2.jpg"
 
 export const team = [
     {
@@ -20,6 +25,9 @@ export const team = [
         role: "Founder Teh Developer & Fullstack Developer",
         quotes: "Saya menciptakan solusi teknis yang kuat dan cepat. Dari ide hingga peluncuran, saya memastikan website dan aplikasi berjalan lancar tanpa bug",
         about: "Learn about myself here",
+         vision1: "Flexibility & Independence",
+        vision2: "Enhance My Soft Skill",
+        vision3: "Building Impactful Digital Solutions",
         mail: "sevalino@gmail.com",
         photo: Sevalino,
         menuPhoto: SevalinoMenu,
@@ -27,11 +35,19 @@ export const team = [
             {
               id: 1,
               picture:IbnuProject1,
+              tag: ["Fullstack", "Laravel", "Solo Project"],
+              name:"TwiiAhh",
+              type:"Social Media Website",
+              description:'Sebuah website social media seperti social media pada umumnya yaitu website yang dia bisa ngescroll, chat, talipun, message, call, dan lain lain',
               link:"Goggle.com"
             },
             {
               id: 2,
               picture:IbnuProject2,
+              tag: ["Fullstack", "Laravel", "Solo Project"],
+              name:"TwiiAhh",
+              type:"Social Media Website",
+              description:'Sebuah website social media seperti social media pada umumnya yaitu website yang dia bisa ngescroll, chat, talipun, message, call, dan lain lain',
               link:"Goggle.com"
             },
         ]
@@ -42,6 +58,9 @@ export const team = [
         role: "Co Founder Teh Developer & Fullstack Developer",
         quotes: "Saya menciptakan solusi teknis yang kuat dan cepat. hingga peluncuran, saya memastikan website dan aplikasi berjalan lancar tanpa bug",
         about: "Learn about myself here",
+         vision1: "Flexibility & Independence",
+        vision2: "Enhance My Soft Skill",
+        vision3: "Building Impactful Digital Solutions",
         mail: "ibnuformal@gmail.com",
         photo: Ibnu,
         menuPhoto: IbnuMenu,
@@ -50,11 +69,19 @@ export const team = [
             {
               id: 1,
               picture:IbnuProject1,
+              tag: ["Fullstack", "Laravel", "Solo Project"],
+              name:"TwiiAhh",
+              type:"Social Media Website",
+              description:'Sebuah website social media seperti social media pada umumnya yaitu website yang dia bisa ngescroll, chat, talipun, message, call, dan lain lain',
               link:"Goggle.com"
             },
             {
               id: 2,
               picture:IbnuProject2,
+              tag: ["Fullstack", "Laravel", "Solo Project"],
+              name:"TwiiAhh",
+              type:"Social Media Website",
+              description:'Sebuah website social media seperti social media pada umumnya yaitu website yang dia bisa ngescroll, chat, talipun, message, call, dan lain lain',
               link:"Goggle.com"
             },
         ]
@@ -65,18 +92,29 @@ export const team = [
         role: "Marketing & UI UX Designer of Teh Developer",
         quotes: "Saya adalah jembatan antara kreativitas dan strategi,  menciptakan pengalaman digital yang tidak hanya indah, tapi juga bermakna. Dari konsep hingga eksekusi, saya memastikan setiap kisah brand hadir dengan jelas, emosional, dan berdampak",
         about: "Learn about myself here",
+         vision1: "Flexibility & Independence",
+        vision2: "Enhance My Soft Skill",
+        vision3: "Building Impactful Digital Solutions",
         mail: "zahir@gmail.com",
         photo: Zahir,
         menuPhoto: ZahirMenu,
          project: [
             {
               id: 1,
-              picture:IbnuProject1,
+              picture:ZahirProject1,
+              tag: ["Fullstack", "Laravel", "Solo Project"],
+              name:"TwiiAhh",
+              type:"Social Media Website",
+              description:'Sebuah website social media seperti social media pada umumnya yaitu website yang dia bisa ngescroll, chat, talipun, message, call, dan lain lain',
               link:"Goggle.com"
             },
             {
               id: 2,
-              picture:IbnuProject2,
+              picture:ZahirProject2,
+              tag: ["Fullstack", "Laravel", "Solo Project"],
+              name:"Kalcer Shop",
+              type:"Social Media Website",
+              description:'Sebuah website social media seperti social media pada umumnya yaitu website yang dia bisa ngescroll, chat, talipun, message, call, dan lain lain',
               link:"Goggle.com"
             },
         ]
@@ -85,20 +123,32 @@ export const team = [
         id: 4,
         name: "Abidal Farzan",
         role: "UI UX Designer & Marketing of Teh Developer",
-        quotes: "Saya menciptakan solusi yang kuat dan cepat. Dari ide hingga peluncuran, saya memastikan website dan aplikasi berjalan lancar tanpa bug",
-        about: "Learn about myself here",
-        mail: "farzan@gmail.com",
+        quotes: "I am Abidal Farzan Rosyidi, a 16–year–old student passionate about exploring the digital world. I am eager to enhance my knowledge in web development, UI/UX, and modern technologies. With curiosity and dedication, I aim to grow my skills and contribute to meaningful digital projects.",
+        about: "Just make it flex",
+        vision1: "Flexibility & Independence",
+        vision2: "Enhance My Soft Skill",
+        vision3: "Building Impactful Digital Solutions",
+        mail: "abidalfarzan02@gmail.com",
         photo: Farzan,
         menuPhoto: FarzanMenu,
+        cv:FarzanCV,
          project: [
             {
               id: 1,
-              picture:IbnuProject1,
+              picture:FarzanProject1,
+              tag: ["Fullstack", "Laravel", "Solo Project"],
+              name:"TwiiAhh",
+              type:"Social Media Website",
+              description:'Sebuah website social media seperti social media pada umumnya yaitu website yang dia bisa ngescroll, chat, talipun, message, call, dan lain lain',
               link:"Goggle.com"
             },
             {
               id: 2,
-              picture:IbnuProject2,
+              picture:FarzanProject2,
+              tag: ["Fullstack", "Laravel", "Solo Project"],
+              name:"TwiiAhh",
+              type:"Social Media Website",
+              description:'Sebuah website social media seperti social media pada umumnya yaitu website yang dia bisa ngescroll, chat, talipun, message, call, dan lain lain',
               link:"Goggle.com"
             },
         ]
--- src/sections/ProjectPortofolio.jsx
+++ src/sections/ProjectPortofolio.jsx
@@ -1,5 +1,7 @@
 import { team } from "../data/team";
 import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { FiArrowRight } from "react-icons/fi";
 
 export default function ProjectPortofolio() {
     const [selected, setSelected] = useState(team[0]);
@@ -32,7 +34,7 @@ export default function ProjectPortofolio() {
                 {/* Content */}
                 <div className="w-full flex justify-center items-center">
                     {/* Upper Project */}
-                    <div className="bg-black rounded-lg p-4 w-[50%] flex gap-6">
+                    <div className="bg-[#363231] rounded-lg p-4 w-[50%] flex gap-6">
                         {/* Gambar */}
                         <div className="w-fit">
                             <img 
@@ -43,32 +45,64 @@ export default function ProjectPortofolio() {
                         </div>
 
                         {/* Desc */}
-                        <div>
-                            {/* Label */}
-                            <div className="flex h-fit gap-2">
-                                <div className="text-white bg-primary px-4 py-2 rounded-full">
-                                    <h1>Fullstack</h1>
+                        <div className="space-y-4">
+                           {/* Label (tags) */}
+                                <div className="flex h-fit gap-2 flex-wrap">
+                                {selected.project[0].tag?.map((t, i) => (
+                                    <div
+                                    key={i}
+                                    className="text-white bg-primary px-4 py-2 rounded-full"
+                                    >
+                                    <h1>{t}</h1>
+                                    </div>
+                                ))}
                                 </div>
-                                <div className="text-white bg-primary px-4 py-2 rounded-full">
-                                    <h1>Laravel</h1>
-                                </div>
-                                <div className="text-white bg-primary px-4 py-2 rounded-full">
-                                    <h1>Solo Project</h1>
-                                </div>
-                            </div>
 
                             {/* Title */}
-                            <div className="text-white">
-                                <h1>TwiiAhh</h1>
-                                <h1>Social Media Website</h1>
+                            <div className="text-white text-2xl font-semibold px-2">
+                                <h1>{selected.project[0].name}</h1>
+                                <h1>{selected.project[0].type}</h1>
                             </div>
 
-                            <div className="text-white w-full max-w-md">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo fugit pariatur quasi soluta architecto veritatis placeat fuga harum sit doloremque!</p>
+                            <div className="text-white w-full max-w-md text-sm px-2 text-justidy">
+                                <p>{selected.project[0].description}</p>
                             </div>
 
-                            <div className="text-white">
-                                <a href="">Test</a>
+                            <div className="text-white border border-white w-fit py-2 rounded-full px-1">
+                                <motion.a
+                                    href="#"
+                                    className="relative flex items-center justify-center bg-[#363231] text-white px-6 py-3 rounded-full cursor-pointer w-40"
+                                    initial="rest"
+                                    whileHover="hover"
+                                    animate="rest"
+                                    >
+                                    {/* Text */}
+                                    <motion.span
+                                        variants={{
+                                            rest: {
+                                            clipPath: "inset(0 100% 0 0)", // ketutup full dari kanan
+                                            },
+                                            hover: {
+                                            clipPath: "inset(0 0% 0 0)", // kebuka full
+                                            transition: { duration: 0.4, ease: "easeInOut" },
+                                            },
+                                        }}
+                                        className="absolute w-fit bg-primary px-2 py-1 left-0 rounded-full overflow-hidden"
+                                        >
+                                        Lihat Detail
+                                        </motion.span>
+
+                                    {/* Arrow */}
+                                    <motion.span
+                                        variants={{
+                                        rest: { x: -20 },
+                                        hover: { x: 100, transition: { duration: 0.4 } }, // geser full ke kanan
+                                        }}
+                                        className="absolute bg-white rounded-full text-black left-6"
+                                    >
+                                        <FiArrowRight size={30} />
+                                    </motion.span>
+                                </motion.a>
                             </div>
                         </div>
                     </div>
