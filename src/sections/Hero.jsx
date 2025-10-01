262b1cc67a66493104aa755ba1d82e7cc9d72444
Sevalino Elfata <sevalinoelfata8@gmail.com>
2025-10-01 09:13:00 +0700

    Add Swiper dependency and fix layout in Hero component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
 return (
     <div className="w-full py-10 font-jakarta">
         <div className="flex flex-col lg:flex-row w-full h-full justify-center items-start space-y-20">
-            <div className="w-full lg:w-1/2 px-5 md:px-20 flex flex-col justify-center items-center md:justify-start space-y-6 md:space-y-14">
+            <div className="w-full lg:w-1/2 px-5 md:px-20 flex flex-col justify-center items-center lg:justify-start space-y-6 md:space-y-14">
                 <h1 className="text-4xl text-center lg:text-start md:text-6xl tracking-tight font-bold leading-[1.3] ">
                     Punya Website Kini Sesantai <br />
                     <span className='text-primary'>
@@ -18,7 +18,7 @@ return (
                 </p>
                 <Link to="/contact" className="w-fit px-4 py-2 md:px-7 md:py-3 bg-primary hover:font-bold ease-in-out text-white rounded-lg font-semibold text:base md:text-xl hover:bg-white hover:text-secondary border-3 border-primary shadow-none  transition-all duration-500">Layanan Kami</Link>
             </div>
-            <div className="w-full lgw-1/2 h-full px-5 md:px-20 flex flex-col  items-end">
+            <div className="w-full lg:w-1/2 h-full px-5 md:px-20 flex flex-col  items-end">
             <div className='absolute px-4 py-3 md:px-6 md:py-5 rounded-e-4xl rounded-tl-4xl flex flex-col gap-2  -translate-y-5 translate-x-5 bg-secondary text-white '>
                 <p className='text-sm md:text-xl font-semibold leading-[1.2]'>
                         Digitalisasi Bisnis Anda 
