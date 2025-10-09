4083fb70d2d43561e19f871c4c9e88d3fa3e698b
Sevalino Elfata <sevalinoelfata8@gmail.com>
2025-10-09 20:55:48 +0700

    Increase padding in NavbarLinkButton for improved aesthetics

--- src/components/NavbarLinkButton.jsx
+++ src/components/NavbarLinkButton.jsx
@@ -6,7 +6,7 @@ function NavbarLinkButton({ href, children }) {
   const [isHover, setIsHover] = useState(false)
   return (
     <motion.div onHoverStart={() => setIsHover(true)} onHoverEnd={() => setIsHover(false)} className="group flex flex-col items-center space-y-1 cursor-pointer">
-      <Link to={href} className={` px-1 rounded-md overflow-hidden whitespace-nowrap h-7 font-medium text-lg font-jakarta  flex flex-col items-center justify-start`}>
+      <Link to={href} className={` px-2 rounded-md overflow-hidden whitespace-nowrap h-7 font-medium text-lg font-jakarta  flex flex-col items-center justify-start`}>
         <motion.div animate={{ height: isHover ? "0%" : "100%" }} className="overflow-hidden whitespace-nowrap flex items-end justify-end">
           {children}
         </motion.div>
